279346a45a36ef4c9b5e89d8dcaa3a144820a333
AwpticGH <rafihariyadi01@gmail.com>
2024-11-08 20:26:56 +0700

    Minor update GetController Changed get_all function to only return data that hasn't been deleted

--- src/controller/GetController.py
+++ src/controller/GetController.py
@@ -8,7 +8,12 @@ class GetController(BaseController):
     super().__init__(todo_list)
   
   def get_all(self):
-    return self.todo_list
+    my_list: List[TodoModel] = []
+    for todo in self.todo_list:
+      if todo.deleted_at is None:
+        my_list.append(todo)
+
+    return my_list
   
   def get_by_id(self, id: int):
     for todo in self.todo_list:
